b1fb5dd9673dd7bc72d3910d6de9974a2b5f6f46
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-07-06 14:26:27 +0200

    Ajout lien editer et supprimer dans la liste des users

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire-M</title>
+    <link rel="stylesheet" href="/asset/style.css">
+</head>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="list_users.php">Liste des utilisateur</a></li>
+        </ul>
+    </nav>
+</header>
+
+<body>
--- index.php
+++ index.php
@@ -1,37 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire-M</title>
-    <link rel="stylesheet" href="/asset/style.css">
-</head>
-<?php
-require('./includes/functions');
+<?php 
 require('./includes/pdo.php');
+require('./includes/functions.php');
+include('./includes/header.php'); 
 
+$success = false;
+$errors = [];
+if(!empty($_POST['submitted'])) {
+
+    $nom = cleanXss('nom');
+    $prenom = cleanXss('prenom');
+    $email = cleanXss('email');
+
+    $errors = validText($errors,$nom,'nom',2,10);
+    $errors = validText($errors,$prenom,'prenom',2,10);
+    $errors = validEmail($errors, $email, 'email');
+
+    if(count($errors) === 0) {
+        $requete_insert = "INSERT INTO users (nom,prenom,email,created_at) VALUES (:nom,:prenom,:email,NOW())";
+        $query = $pdo->prepare($requete_insert);
+        $query->bindValue(':nom',$nom, PDO::PARAM_STR);
+        $query->bindValue(':prenom',$prenom, PDO::PARAM_STR);
+        $query->bindValue(':email',$email, PDO::PARAM_STR);
+        $query->execute();
+        header('Location: list_users.php');
+    }
+}
 ?>
-<body>
-    
-<form action="" method="POST" class="wrap2">
+    <form action="" method="post" class="wrap2" novalidate>
+        <label for="nom">
+            <span>Nom :</span>
+            <input type="text" name="nom" value="<?php if(!empty($_POST['nom'])) { echo $_POST['nom']; } ?>">
+            <span class="error"><?php if(!empty($errors['nom'])) { echo $errors['nom']; } ?></span>
 
-<label for="nom">Nom</label>
-<input type="text" name="nom" id="nom">
-
-<label for="prenom">Prénom</label>
-<input type="text" name="prenom" id="prenom">
-
-<label for="email">Email</label>
-<input type="email" name="email" id="email">
-
-<input type="submit" name="submit" value="Ajouter un utilisateur">
-
-
-
-</form>
-
-
-
-</body>
-</html>
+        </label>
+        <label for="prenom">
+            <span>Prenom :</span>
+            <input type="text" name="prenom" value="<?php if(!empty($_POST['prenom'])) { echo $_POST['prenom']; } ?>">
+            <span class="error"><?php if(!empty($errors['prenom'])) { echo $errors['prenom']; } ?></span>
+        </label>
+        <label for="email">
+            <span>Email :</span>
+            <input type="text" name="email" value="<?php if(!empty($_POST['email'])) { echo $_POST['email']; } ?>">
+            <span class="error"><?php if(!empty($errors['email'])) { echo $errors['email']; } ?></span>
+        </label>
+        <input type="submit" name="submitted" value="Ajouter un nouvel utilisateur">
+    </form>
+<?php include('./includes/footer.php'); ?>
