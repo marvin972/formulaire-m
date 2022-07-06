b6fc3752d3c3452320b9e547765232b2006ee960
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-07-06 10:12:08 +0200

    prepare insert

--- index.php
+++ index.php
@@ -5,12 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire-M</title>
+    <link rel="stylesheet" href="/asset/style.css">
 </head>
+<?php
+require('./includes/functions');
+require('./includes/pdo.php');
+
+?>
 <body>
     
-<form action="">
+<form action="" method="POST" class="wrap2">
 
+<label for="nom">Nom</label>
+<input type="text" name="nom" id="nom">
 
+<label for="prenom">Prénom</label>
+<input type="text" name="prenom" id="prenom">
+
+<label for="email">Email</label>
+<input type="email" name="email" id="email">
+
+<input type="submit" name="submit" value="Ajouter un utilisateur">
 
 
 
